fc6eab62d9bb7602ba37f0bcdb16bfe8985a75e0
TotoroDavid <davidtoro65@hotmail.com>
2021-06-24 16:28:46 -0500

    finished the project

---
+++ README.md
@@ -0,0 +1,7 @@
+# to send Email using Node.js
+
+in the next project, I will make a simple app to send emails from the Node application. Being able to send emails from our website or app is an important topic in web development.
+how to send emails from a node app using SendGrid as our mailing server. SendGrid is a great service and really easy to use and integrate into our Node apps.
+
+<img src="">
+<img src="">
--- server.js
+++ server.js
@@ -1,19 +1,43 @@
-const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(
-    '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'
-)
+const express = require('express')
+const path = require('path')
+const sendemail = require('./utils/sendEmail')
 
-const msg = {
-    to: 'leo63321616@gmail.com',
-    from: 'davidtoro65@hotmail.com',
-    subject: 'testing node email service',
-    text: 'this is awesome email sent from node app'
-}
+const app = express()
 
-sgMail.send(msg, function(err, info) {
-    if (err) {
-        console.log('email not sent')
-    } else {
-        console.log('email sent success')
-    }
+app.use(express.urlencoded({ extended: false }))
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('contact')
 })
+
+app.get('/sent', (req, res) => {
+    res.render('sent')
+})
+
+app.post('/sendemail', (req, res) => {
+    const { name, surname, email } = req.body
+
+    const from = 'davidtoro65@hotmail.com'
+    const to = 'leo63321616@gmail.com'
+    const subject = "New Contact Request"
+
+    const output = `
+        <p> you have a new contact request </p>
+        <h3> contact details </h3>
+        <ul>
+            <li> Name :${name} </li>
+            <li> Surname :${surname} </li>
+            <li> Email :${email} </li>
+        </ul>
+    `
+    sendemail(to, from, subject, output)
+    res.redirect("/sent");
+
+
+})
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
